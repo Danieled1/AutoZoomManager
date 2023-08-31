55aa345e685e5ded6991e322126abc94d302836f
danieled1 <djping43@gmail.com>
2023-08-31 15:36:24 +0300

    Testing Sessions on MongoDB3

--- server-to-server-oauth-starter-api-main/routes/api/webhooks.js
+++ server-to-server-oauth-starter-api-main/routes/api/webhooks.js
@@ -2,19 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/meeting-ended", async (req, res) => {
-  // const zoomToken = req.headerConfig["Authorization"]; // This is a hypothetical header. Check Zoom's documentation for the exact header name.
+  const zoomToken = req.headerConfig["Authorization"]; // This is a hypothetical header. Check Zoom's documentation for the exact header name.
 
   // // Verify the token
   // if (zoomToken !== process.env.ZOOM_VERIFICATION_TOKEN) {
   //   return res.status(401).send("Unauthorized request");
   // }
-  console.log(req.body);
-  const { payload } = req.body;
-  const meetingId = payload.object.id;
 
-  // Now find the user with this meeting ID and update their record.
-  // ...
-
+  const { payload, event } = req.body;
+  if (event === "endpoint.url_validation") {
+    // Handle URL validation here
+    return res.status(200).send();
+  }
+  let meetingId;
+  if (payload && payload.object) {
+    meetingId = payload.object.id;
+  }
+  console.log(payload, meetingId);
+  // if (meetingId) {
+  //   // Now find the user with this meeting ID and update their record.
+  //   // ...
+  // }
   res.status(200).send();
 });
 
