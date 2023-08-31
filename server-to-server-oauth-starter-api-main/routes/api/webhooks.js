f575fcf0c8d8faaddc82d2d34f147596f548a10d
danieled1 <djping43@gmail.com>
2023-08-31 16:00:34 +0300

    Testing Sessions on MongoDB. Validating URL for webhook

--- server-to-server-oauth-starter-api-main/routes/api/webhooks.js
+++ server-to-server-oauth-starter-api-main/routes/api/webhooks.js
@@ -1,23 +1,47 @@
 const express = require("express");
 const router = express.Router();
-
+const crypto = require("crypto");
 router.post("/meeting-ended", async (req, res) => {
-  const { payload, event } = req.body;
+  const { event, payload, event_ts } = req.body;
 
+  // For Challenge-Response Check (CRC)
   if (event === "endpoint.url_validation") {
-    // Handle URL validation here
-    console.log("ENDPOINT.URL_VALIDATON SUCCESS");
-    return res.status(200).send();
+    const hashForValidate = crypto
+      .createHmac("sha256", process.env.ZOOM_WEBHOOK_SECRET_TOKEN)
+      .update(payload.plainToken)
+      .digest("hex");
+
+    return res.status(200).json({
+      plainToken: payload.plainToken,
+      encryptedToken: hashForValidate,
+    });
   }
+
+  // For HMAC SHA-256 Hashing
+  const message = `v0:${req.headers["x-zm-request-timestamp"]}:${JSON.stringify(
+    req.body
+  )}`;
+  const hashForVerify = crypto
+    .createHmac("sha256", process.env.ZOOM_WEBHOOK_SECRET_TOKEN)
+    .update(message)
+    .digest("hex");
+
+  const signature = `v0=${hashForVerify}`;
+
+  if (req.headers["x-zm-signature"] !== signature) {
+    return res.status(401).send("Unauthorized request");
+  }
+
+  // Your existing logic here
   let meetingId;
   if (payload && payload.object) {
     meetingId = payload.object.id;
   }
-  console.log(payload, meetingId);
-  // if (meetingId) {
-  //   // Now find the user with this meeting ID and update their record.
-  //   // ...
-  // }
+  if (meetingId) {
+    // Find the user with this meeting ID and update their record.
+    // ...
+  }
+
   res.status(200).send();
 });
 
