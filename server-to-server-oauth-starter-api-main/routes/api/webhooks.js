bfd238ad2e8041b891ff5d850d66469762b222e9
danieled1 <djping43@gmail.com>
2023-09-04 12:38:17 +0300

    Added more logs for further debugging :)

--- server-to-server-oauth-starter-api-main/routes/api/webhooks.js
+++ server-to-server-oauth-starter-api-main/routes/api/webhooks.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const crypto = require("crypto");
+
 router.post("/meeting-ended", async (req, res) => {
   try {
-    console.log("Received request:", req.body);
+    // Log request body
+    console.log("Received request body:", req.body);
+    
+    // Log request headers
+    console.log("Received request headers:", req.headers);
 
+    // Check if the ZOOM_SECRET_TOKEN is available
     if (!process.env.ZOOM_SECRET_TOKEN) {
-      return res
-        .status(500)
-        .send("Server configuration error: ZOOM_WEBHOOK_SECRET_TOKEN not set.");
+      console.error("ZOOM_SECRET_TOKEN not set.");
+      return res.status(500).send("Server configuration error: ZOOM_SECRET_TOKEN not set.");
     }
 
     const { event, payload } = req.body;
-    const message = `v0:${
-      req.headers["x-zm-request-timestamp"]
-    }:${JSON.stringify(req.body)}`;
+    const message = `v0:${req.headers["x-zm-request-timestamp"]}:${JSON.stringify(req.body)}`;
 
     const hashForVerify = crypto
       .createHmac("sha256", process.env.ZOOM_SECRET_TOKEN)
@@ -22,6 +25,10 @@ router.post("/meeting-ended", async (req, res) => {
       .digest("hex");
 
     const signature = `v0=${hashForVerify}`;
+
+    // Log expected and received signatures
+    console.log(`Expected Signature: ${signature}`);
+    console.log(`Received Signature: ${req.headers["x-zm-signature"]}`);
 
     if (req.headers["x-zm-signature"] !== signature) {
       return res.status(401).send("Unauthorized request");
@@ -40,13 +47,18 @@ router.post("/meeting-ended", async (req, res) => {
         },
       });
     } else {
+      // Log when the event is not as expected
+      console.log(`Unexpected event: ${event}`);
+      
       res.status(401).json({
         message: "Unauthorized request to Zoom Webhook",
       });
     }
   } catch (error) {
     console.error("An error occurred:", error);
-    res.status(500).send("Internal Server Error");
+    
+    // Send detailed error message in the response
+    res.status(500).send(`Internal Server Error: ${error.message}`);
   }
 });
 
