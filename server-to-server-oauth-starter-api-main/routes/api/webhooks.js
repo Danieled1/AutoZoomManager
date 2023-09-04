b19c672e171af4cf8dd1cd9241b5d40dcd8d5cbc
danieled1 <djping43@gmail.com>
2023-09-04 12:41:50 +0300

    Added more logs for further debugging 2 :)

--- server-to-server-oauth-starter-api-main/routes/api/webhooks.js
+++ server-to-server-oauth-starter-api-main/routes/api/webhooks.js
@@ -1,36 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const crypto = require("crypto");
-
 router.post("/meeting-ended", async (req, res) => {
   try {
-    // Log request body
     console.log("Received request body:", req.body);
-    
-    // Log request headers
     console.log("Received request headers:", req.headers);
 
-    // Check if the ZOOM_SECRET_TOKEN is available
     if (!process.env.ZOOM_SECRET_TOKEN) {
-      console.error("ZOOM_SECRET_TOKEN not set.");
-      return res.status(500).send("Server configuration error: ZOOM_SECRET_TOKEN not set.");
+      console.log(
+        "Server configuration error: ZOOM_WEBHOOK_SECRET_TOKEN not set."
+      );
+      return res
+        .status(500)
+        .send("Server configuration error: ZOOM_WEBHOOK_SECRET_TOKEN not set.");
     }
 
     const { event, payload } = req.body;
-    const message = `v0:${req.headers["x-zm-request-timestamp"]}:${JSON.stringify(req.body)}`;
+    const message = `v0:${
+      req.headers["x-zm-request-timestamp"]
+    }:${JSON.stringify(req.body)}`;
 
     const hashForVerify = crypto
       .createHmac("sha256", process.env.ZOOM_SECRET_TOKEN)
       .update(message)
       .digest("hex");
-
     const signature = `v0=${hashForVerify}`;
 
-    // Log expected and received signatures
-    console.log(`Expected Signature: ${signature}`);
-    console.log(`Received Signature: ${req.headers["x-zm-signature"]}`);
+    console.log("Expected Signature:", signature);
+    console.log("Received Signature:", req.headers["x-zm-signature"]);
 
     if (req.headers["x-zm-signature"] !== signature) {
+      console.log("Unauthorized request");
       return res.status(401).send("Unauthorized request");
     }
 
@@ -40,25 +40,22 @@ router.post("/meeting-ended", async (req, res) => {
         .update(payload.plainToken)
         .digest("hex");
 
-      res.status(200).json({
+      return res.status(200).json({
         message: {
           plainToken: payload.plainToken,
           encryptedToken: hashForValidate,
         },
       });
     } else {
-      // Log when the event is not as expected
-      console.log(`Unexpected event: ${event}`);
-      
-      res.status(401).json({
+      // Handle other events
+      console.log("Unauthorized request to Zoom Webhook");
+      return res.status(401).json({
         message: "Unauthorized request to Zoom Webhook",
       });
     }
   } catch (error) {
     console.error("An error occurred:", error);
-    
-    // Send detailed error message in the response
-    res.status(500).send(`Internal Server Error: ${error.message}`);
+    res.status(500).send("Internal Server Error");
   }
 });
 
