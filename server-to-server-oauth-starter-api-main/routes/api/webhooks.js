b9ab4df770736d04999d83e4a90a69cbdbc7b563
danieled1 <djping43@gmail.com>
2023-09-04 12:10:45 +0300

    Debugging a bug of undefined type on webhooks 2

--- server-to-server-oauth-starter-api-main/routes/api/webhooks.js
+++ server-to-server-oauth-starter-api-main/routes/api/webhooks.js
@@ -1,39 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const crypto = require("crypto");
-router.post("/meeting-ended", async (req, res) => {
-  const { event, payload, event_ts } = req.body;
-  // For HMAC SHA-256 Hashing
-  const message = `v0:${req.headers["x-zm-request-timestamp"]}:${JSON.stringify(
-    req.body
-  )}`;
-  console.log( process.env.ZOOM_WEBHOOK_SECRET_TOKEN);
-  const hashForVerify = crypto
-    .createHmac("sha256", process.env.ZOOM_WEBHOOK_SECRET_TOKEN)
-    .update(message)
-    .digest("hex");
+// router.post("/meeting-ended", async (req, res) => {
+//   const { event, payload, event_ts } = req.body;
+//   // For HMAC SHA-256 Hashing
+//   const message = `v0:${req.headers["x-zm-request-timestamp"]}:${JSON.stringify(
+//     req.body
+//   )}`;
+//   console.log( process.env.ZOOM_WEBHOOK_SECRET_TOKEN);
+//   const hashForVerify = crypto
+//     .createHmac("sha256", process.env.ZOOM_WEBHOOK_SECRET_TOKEN)
+//     .update(message)
+//     .digest("hex");
 
-  const signature = `v0=${hashForVerify}`;
+//   const signature = `v0=${hashForVerify}`;
 
-  if (req.headers["x-zm-signature"] !== signature) {
-    return res.status(401).send("Unauthorized request");
-  }
-  if(event === "endpoint.url_validation"){
-    const hashForValidate = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(payload.plainToken).digest('hex')
-    console.log(hashForValidate, "hashForValidate");
-    res.status(200).json({
-      message: {
-        plainToken: payload.plainToken,
-        encryptedToken: hashForValidate
-      }
-    });
-  } else {
-    res.status(401).json({
-      message: "Unauthorized request to Zoom Webhook",
-    });
-  }
+//   if (req.headers["x-zm-signature"] !== signature) {
+//     return res.status(401).send("Unauthorized request");
+//   }
+//   if(event === "endpoint.url_validation"){
+//     const hashForValidate = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(payload.plainToken).digest('hex')
+//     console.log(hashForValidate, "hashForValidate");
+//     res.status(200).json({
+//       message: {
+//         plainToken: payload.plainToken,
+//         encryptedToken: hashForValidate
+//       }
+//     });
+//   } else {
+//     res.status(401).json({
+//       message: "Unauthorized request to Zoom Webhook",
+//     });
+//   }
 
   
-});
+// });
 
 module.exports = router;
